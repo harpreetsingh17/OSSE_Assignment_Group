280f6d9fd8ee57cb22a4df6ff26ef8d1b820526c
Noirheeta Chanda <noirheeta.chanda@sap.com>
2021-12-14 19:24:56 +0530

    Adding initial files

---
+++ learning_lil/src/test/java/com/linkedinlearning/learning_lil/LearningLilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedinlearning.learning_lil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningLilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
