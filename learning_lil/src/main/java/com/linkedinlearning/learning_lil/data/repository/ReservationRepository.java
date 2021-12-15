2e5df2c38e9c3ea6e0ecc8dc042b487ad6c890c8
Noirheeta Chanda <noirheeta.chanda@sap.com>
2021-12-15 19:13:45 +0530

    added repository class for room reservation

---
+++ learning_lil/src/main/java/com/linkedinlearning/learning_lil/data/repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.linkedinlearning.learning_lil.data.repository;
+
+import com.linkedinlearning.learning_lil.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
