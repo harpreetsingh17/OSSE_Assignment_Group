6a0fed2d25a5482b680fb01b64c12b913843bcad
Noirheeta Chanda <noirheeta.chanda@sap.com>
2021-12-15 19:23:33 +0530

    Added room repo

--- learning_lil/src/main/java/com/linkedinlearning/learning_lil/data/repository/ReservationRepository.java
+++ learning_lil/src/main/java/com/linkedinlearning/learning_lil/data/repository/ReservationRepository.java
@@ -4,7 +4,6 @@ import com.linkedinlearning.learning_lil.data.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
 
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
